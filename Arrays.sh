38c25549752b984144bb04ae2fcd70d357fbb3b3
tejastn10 <tejastn10@gmail.com>
2020-07-28 16:31:40 +0530

    📦 ADD: For loop

--- Arrays.sh
+++ Arrays.sh
@@ -8,3 +8,10 @@ NUMBER_OF_STATES=${#STATES[@]}
 
 echo "I live in " ${COUNTRIES[2]}
 echo "It has more than $NUMBER_OF_STATES states."
+
+# Print elements of array using for
+a=(3 5 8 10 6) 
+
+for x in "${a[@]}"; do 
+  echo $x
+done
